cf067ae1ce7902b0ed17a5e4d2868984a05daf4a
medicsera <anton.mirko.0550@gmail.com>
2025-10-15 13:15:27 +0700

    done task 1 lab3

--- main/java/com/example/firstapp/MainActivity.kt
+++ main/java/com/example/firstapp/MainActivity.kt
@@ -5,20 +5,54 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstapp.ui.theme.FirstAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.actitity_main)
+        setContent{
+            ArticleScreen()
+        }
+
+    }
+}
+
+@Composable
+fun ArticleScreen(){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp)
+    ){
+        Image(painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .height(180.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = stringResource(R.string.article_title))
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = stringResource(R.string.article_intro))
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = stringResource(R.string.article_body))
 
     }
 }
