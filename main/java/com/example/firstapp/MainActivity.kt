97e712659dfe6b1f0b659cef3ddf1e8a18145a77
medicsera <anton.mirko.0550@gmail.com>
2025-10-15 14:00:06 +0700

    done task 3 lab3

--- main/java/com/example/firstapp/MainActivity.kt
+++ main/java/com/example/firstapp/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -18,15 +21,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.firstapp.ui.theme.FirstAppTheme
+
 
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +44,33 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+    }
+}
+
+val cellColors = listOf(
+    Color(0xFFEADDFF),
+    Color(0xFFD0BCFF),
+    Color(0xFFB69DF8),
+    Color(0xFFF6EDFF)
+)
+
+@Composable
+fun AppNav() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "article") {
+        composable("article") {
+            ArticleScreen(onNavigate = { navController.navigate("completed") })
+        }
+        composable("completed") {
+            CompletedScreen(
+                onNavigateToGrid = { navController.navigate("grid") },
+                onNavigateBack = { navController.popBackStack() }
+
+            )
+        }
+        composable("grid") {
+            InfoGrid(onNavigateBack = { navController.popBackStack() })
+        }
     }
 }
 
@@ -72,7 +105,7 @@ fun ArticleScreen(onNavigate: () -> Unit) {
 
 
 @Composable
-fun CompletedScreen() {
+fun CompletedScreen(onNavigateToGrid: () -> Unit, onNavigateBack: () -> Unit) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -97,13 +130,86 @@ fun CompletedScreen() {
             )
         }
     }
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomStart) {
+        Button(onClick = onNavigateBack) {
+            Text("<-")
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
+        Button(onClick = onNavigateToGrid) {
+            Text("->")
+        }
+    }
 }
 
 @Composable
-fun AppNav() {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "article") {
-        composable("article") { ArticleScreen(onNavigate = { navController.navigate("completed") }) }
-        composable("completed") { CompletedScreen() }
+fun InfoCell(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String,
+    background: Color
+) {
+    Box(
+        modifier = modifier
+            .padding(16.dp)
+            .background(background)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = description,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+
+}
+
+@Composable
+fun InfoGrid(onNavigateBack: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.weight(1f)) {
+            InfoCell(
+                modifier = Modifier.weight(1f),
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                background = cellColors[0]
+            )
+            InfoCell(
+                modifier = Modifier.weight(1f),
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                background = cellColors[1]
+            )
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            InfoCell(
+                modifier = Modifier.weight(1f),
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                background = cellColors[2]
+            )
+            InfoCell(
+                modifier = Modifier.weight(1f),
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                background = cellColors[3]
+            )
+        }
+    }
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomStart){
+        Button(onClick = onNavigateBack) {
+            Text("<-")
+        }
     }
 }
